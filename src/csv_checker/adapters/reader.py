5d6beec6c7025dc734e547db069a946b94400a17
Karson Bryant <karson@karsonbryant.com>
2026-02-03 17:32:09 -0600

    Created: reader class for yielding data to the profiler

---
+++ src/csv_checker/adapters/reader.py
@@ -0,0 +1,11 @@
+import csv
+from pathlib import Path
+
+class Reader:
+    
+    @staticmethod
+    def read(datafile:Path):
+        with datafile.open(mode='r', encoding='utf-8', newline="") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                yield row
